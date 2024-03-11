3799506aed34084a0235d8dd679f191e32bf4f6b
Mob <162936851+pyMob@users.noreply.github.com>
2024-03-10 21:14:41 -0600

    Add files via upload

---
+++ Mob_Starter.py
@@ -0,0 +1,159 @@
+# variable = a reusable container for storing a value
+#            a variable behaves as if it were the value it contains
+
+#age = 19
+
+#print ("you are " + str(age) + " years old")
+#print(f"You are {age} years old")
+
+# INTERGER
+
+#age = 19
+#players = 2
+#quantity = 5
+
+#print(f"you are {age}  years old")
+#print(f"there are {players} players online")
+#print(f"you would like to buy {quantity} items")
+
+# FLOAT
+
+#gpa = 3.2
+#distance = 2.5
+#price = "$10.99"
+
+#print(f"your gpa is {gpa}")
+#print(f"you ran {distance}km")
+#print(f"your price is {price}")
+
+# STRING
+
+#name = "Devyon"
+#food = "Pizza"
+#email = "carrdevyon@icloud.com"
+#print(f"hello {name} is your favorite food {food}?")
+#print(f"Hello {name} please verify your email is {email}")
+#print(f"Shake that ass if your email is {email}")
+
+# BOOLEAN
+
+#online = False
+#for_sale = False
+#running = True
+#Twerking = False
+
+#print("Are you online?: {online}")
+#print(f"Is this item for sale?: {for_sale}")
+#print(f"Is python running?: {running}")
+#print(f"Is man is twerking: {Twerking}")
+
+#if running: 
+     #print("The game is running")   
+#else:
+    #print("The game is over")
+
+#if online:  
+    #print("The game is active")
+#else: 
+    #print(f"The game is dead")
+
+#if Twerking:
+    #print(f"The man is Gay")
+#else:
+    #print("The man is straight")
+
+#x = 1 
+#y = 2
+#z= 3
+
+#print(x)
+#print(y)
+#print(x)
+
+#x = y = z = 0 
+
+#print(x)
+#print(y)
+#print(z)
+
+#x = y = z = 1 
+
+#print(x)
+#print(y)
+#print(z)
+
+
+#Married = True
+
+#print(f"are you married?: {Married}")
+
+#if Married:
+    #"you are married"
+#else:
+    #"you are not married"
+
+#Gay = False
+
+#print(f"are you gay?: {Gay}")
+
+#if Gay:
+    #"you are Gay"
+#else:
+    #"you are not Gay"
+
+
+# typecasting = The process of converting a value of one data type to another
+#                (string, integer, float, boolean)
+#                Explicit vs Implicit
+
+#float = 3.8
+
+#float = int(float)
+#print(float)
+
+#int = 3
+#int = float(int)
+#print(int)
+
+#name = "Devyon"
+#age = 19
+#gpa = 3.8
+#student = True
+
+#print(type(name))
+#print(type(age))
+#print(type(gpa))
+#print(type(student))
+
+#age = float(age)
+#print(age)
+#print(type(age))
+
+#gpa = int(gpa)
+#print(gpa)
+#print(type (gpa))
+
+#student = str(student)
+#print(student)
+
+#print(type(student))
+
+#age = bool(age)
+#print(age)
+
+#name = bool(name)
+#print(name)
+
+#x = 2 
+#y = 2.0
+
+#x = x / y
+
+#print(x)
+
+#^ Implicit makes a value convert automatically ^
+
+Name = "Devyon"
+
+Name = bool(Name)
+print(Name)
