10e489f8937c8ecfe89744fb1da51185c37cbbd2
Mob <162936851+pyMob@users.noreply.github.com>
2024-03-10 21:14:31 -0600

    Add files via upload

---
+++ MobMain.py
@@ -0,0 +1,12 @@
+
+name = input("Enter Your Name: ")
+#age = input("Enter Your Age: ")
+sex = input("Enter Your Sex: ")
+age = int(input("Enter Your Age: "))
+#age = int(age)
+age = age + 1
+print(f"hello {name}.")
+
+print(f"You're {age} years old.")
+
+print(f"You're a {sex}.")
