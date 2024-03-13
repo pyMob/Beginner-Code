b92e399228217c69bc54bd3e8b33e849acf3bc50
Mob <162936851+pyMob@users.noreply.github.com>
2024-03-12 22:20:44 -0600

    Add files via upload

---
+++ MobIfStatements.py
@@ -0,0 +1,28 @@
+# if = do some code IF some condition is True
+#      Else do something else
+# Welcome = input("Would you like to sign up?: ")
+
+# age = int(input('Enter your age: '))
+
+# if age >= 100:
+#    print(f"You are too old to sign up")
+# elif age >= 18:
+#    print(f"You are now signed up")
+# elif age < 0: 
+#    print(f"You havent been born yet!")
+# else:
+#    print(f"Were unable to sign you up")
+
+# response = input("Would you like to eat?: (Y/N): ")
+
+# if response == "Y":
+#    print("have some food then buddy :) ")
+# else:
+#    print("No food for you man >:(")
+
+name = input("Enter your name: ")
+
+if name == "":
+    print("YOU DIDNT PUT A NAME!")
+else:
+    print(f"Hello {name}, Nice to meet you")
