c8372b32eb06399af44e273250a79064dd0737b2
Mob <162936851+pyMob@users.noreply.github.com>
2024-03-21 22:19:40 -0600

    Delete Mob2AreaCalculator.py

--- Mob2AreaCalculator.py
+++
@@ -1,8 +0,0 @@
-
-length = float(input("Enter the length of a Rectangle: "))
-width = float(input ("Enter the width of a Rectangle: "))
-
-area = length * width 
-
-#dont for get (f" ")
-print(f"The area is: {area}cm^2")
